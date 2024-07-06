204b21ce4581f8df68a43a274c16514d0f1cb832
ApenasOLinco <lincoln.matos1003@gmail.com>
2024-07-06 15:21:36 -0300

    Changed InputManager's general purpose.
    Before this commit, the class was responsible for processing the inputted files and passing them to the program's OutputManager. Now, it's purpose is to process inputted files and to buffer them before the user decides what to do. It also holds a reference to the program's InputNotifier, an implementation of the observer pattern to facilitate communication between the program's UI components.

--- src/io/InputManager.java
+++ src/io/InputManager.java
@@ -1,5 +1,9 @@
 package io;
 
+import io.event.InputNotifier;
+import io.event.InputNotifier.EventType;
+import org.jetbrains.annotations.NotNull;
+
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
 import javax.imageio.stream.ImageInputStream;
@@ -7,38 +11,45 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class InputManager {
-    private static final File INPUT_FILE = main.App.getInputFolder();
-    
-    private final OutputManager outputManager = new OutputManager();
+    private final ArrayList<File> inputFiles = new ArrayList<>();
 
-    private final File[] gifFiles;
-    
-    private final HashMap<Image[], String> bufferedGIFs;
+    private final InputNotifier inputNotifier = new InputNotifier();
 
-    public InputManager() {
-        gifFiles = INPUT_FILE.listFiles(f -> f.getName().endsWith(".gif"));
-        assert(gifFiles != null) : "gifFiles variable is null!";
-        bufferedGIFs = new HashMap<>(gifFiles.length);
+    public boolean addFiles(File... files) {
+        return addFiles(null, files);
     }
-    
-    public void process() {
-        try {
-            ImageReader reader = ImageIO.getImageReadersByFormatName("gif").next();
-            
-            for(File gif : gifFiles) {
-                bufferedGIFs.put(bufferGIF(gif, reader), gif.getName().replace(".gif", ""));;
-                outputManager.process(bufferedGIFs, reader);
+
+    public boolean addFiles(String[] formats, File @NotNull ... files) {
+        int addedFiles = 0;
+
+        for(var file : files) {
+            if(!file.getName().endsWith("gif")) {
+                System.out.printf("File %s is not a GIF file. Therefore, it will not be added.\n", file.getName());
+                continue;
             }
-            reader.dispose();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            if(inputFiles.contains(file)) {
+                System.out.printf("Tried to add a duplicate file into input: %s.\n", file.getName());
+                continue;
+            }
+
+            if(inputFiles.add(file))
+                addedFiles++;
         }
+
+
+        if(addedFiles > 0) {
+            inputNotifier.notifyObservers(EventType.FILE_ADDED, files);
+            return true;
+        }
+
+        return false;
     }
     
-    private Image[] bufferGIF(File gif, ImageReader reader) throws IOException {
+	private Image[] bufferGIF(File gif, ImageReader reader) throws IOException {
         ImageInputStream input = ImageIO.createImageInputStream(gif);
         reader.setInput(input, false);
 
@@ -48,9 +59,17 @@ public class InputManager {
         for(int i = 0; i < gifLength; i++) {
             BufferedImage image = reader.read(i);
             images[i] = image;
-            System.out.println(image.getWidth() + "x" + image.getHeight());
+            System.out.println(STR."\{image.getWidth()}x\{image.getHeight()}");
         }
         
         return images;
+    }
+
+    public ArrayList<File> getInputFiles() {
+        return inputFiles;
+    }
+
+    public InputNotifier getInputNotifier() {
+        return inputNotifier;
     }
 }
