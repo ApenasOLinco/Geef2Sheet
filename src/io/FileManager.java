87c0e0c57bc95e4fa5e677e73c6648859779d617
ApenasOLinco <lincoln.matos1003@gmail.com>
2024-07-08 00:00:14 -0300

    Indentation changes

--- src/io/FileManager.java
+++ src/io/FileManager.java
@@ -7,58 +7,45 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class FileManager {
-    private final String INPUT_PATH = "io/input";
-    private final String OUTPUT_PATH = "io/output";
-    private final File INPUT_FOLDER = new File(INPUT_PATH);
-    private final File OUTPUT_FOLDER = new File(OUTPUT_PATH);
-
-    public void createIOFiles() {
-        if (!INPUT_FOLDER.exists()) 
-            System.out.println(INPUT_FOLDER.mkdirs());
-
-        if (!OUTPUT_FOLDER.exists())
-            System.out.println(OUTPUT_FOLDER.mkdirs());
-    }
-    
-    public Image getFileAsImage(File file) {
-        Image image;
-
-        try {
-            image = ImageIO.read(file);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return image;
-    }
-    
-    public String getInputPath() {
-        return INPUT_PATH;
-    }
-
-    public String getOutputPath() {
-        return OUTPUT_PATH;
-    }
-
-    public File getInputFolder() {
-        return INPUT_FOLDER;
-    }
-
-    public File getOutputFolder() {
-        return OUTPUT_FOLDER;
-    }
-
+	private final String INPUT_PATH = "io/input";
+	private final String OUTPUT_PATH = "io/output";
+	private final File INPUT_FOLDER = new File(INPUT_PATH);
+	private final File OUTPUT_FOLDER = new File(OUTPUT_PATH);
+	
+	public void createIOFiles() {
+		if (!INPUT_FOLDER.exists())
+			System.out.println(INPUT_FOLDER.mkdirs());
+		
+		if (!OUTPUT_FOLDER.exists())
+			System.out.println(OUTPUT_FOLDER.mkdirs());
+	}
+	
+	public Image getFileAsImage(File file) {
+		Image image;
+		
+		try {
+			image = ImageIO.read(file);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return image;
+	}
+	
+	public String getInputPath() {
+		return INPUT_PATH;
+	}
+	
+	public String getOutputPath() {
+		return OUTPUT_PATH;
+	}
+	
+	public File getInputFolder() {
+		return INPUT_FOLDER;
+	}
+	
+	public File getOutputFolder() {
+		return OUTPUT_FOLDER;
+	}
+	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/ui/UploadPanel.java
+++ src/ui/UploadPanel.java
@@ -2,8 +2,7 @@ package ui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
@@ -24,20 +23,19 @@ import javax.swing.JSplitPane;
 import javax.swing.SwingConstants;
 import javax.swing.TransferHandler;
 
-import io.event.InputNotifier.EventType;
 import main.App;
 
 public class UploadPanel extends JPanel {
-
+	
 	private UploadLeftPanel leftPanel;
 	private JPanel displayPanel;
 	private JSplitPane splitPane;
-
+	
 	UploadPanel() {
 		initComponents();
 		initTransferHandling();
 	}
-
+	
 	private void initComponents() {
 		setLayout(new BorderLayout());
 		initSplitPane();
@@ -45,40 +43,40 @@ public class UploadPanel extends JPanel {
 		initLeftPanel();
 		initDisplayPanel();
 	}
-
+	
 	private void initTransferHandling() {
 		initDropTarget();
 		initTransferHandler();
 	}
-
+	
 	private void initDropTarget() {
 		this.setDropTarget(new DropTarget(this, new DropTargetAdapter() {
 			@Override
 			public void drop(DropTargetDropEvent dtde) {
 				// Drop validation
 				final TransferHandler.TransferSupport transferSupport = new TransferHandler.TransferSupport(
-						UploadPanel.this, dtde.getTransferable());
-
+				UploadPanel.this, dtde.getTransferable());
+				
 				if (getTransferHandler().canImport(transferSupport))
 					dtde.acceptDrop(DnDConstants.ACTION_COPY);
 				else {
 					dtde.rejectDrop();
 					return;
 				}
-
+				
 				// Import
 				getTransferHandler().importData(UploadPanel.this, dtde.getTransferable());
 			}
 		}));
 	}
-
+	
 	private void initTransferHandler() {
 		this.setTransferHandler(new TransferHandler(null) {
 			@Override
 			public boolean canImport(TransferSupport support) {
 				return Arrays.stream(support.getDataFlavors()).anyMatch(DataFlavor::isFlavorJavaFileListType);
 			}
-
+			
 			@Override
 			public boolean importData(JComponent comp, Transferable t) {
 				try {
@@ -89,22 +87,22 @@ public class UploadPanel extends JPanel {
 						return false;
 				} catch (UnsupportedFlavorException e) {
 					System.err.printf("""
-							Tried getting transfer data of an invalid Flavor while importing %s.
-							   Tried flavor: %s
-							   Supported flavors: %s
-							""", t, DataFlavor.javaFileListFlavor, Arrays.toString(t.getTransferDataFlavors()));
-
+					Tried getting transfer data of an invalid Flavor while importing %s.
+					   Tried flavor: %s
+					   Supported flavors: %s
+					""", t, DataFlavor.javaFileListFlavor, Arrays.toString(t.getTransferDataFlavors()));
+					
 					return false;
 				} catch (IOException e) {
 					e.printStackTrace();
 					return false;
 				}
-
+				
 				return true;
 			}
 		});
 	}
-
+	
 	private void initTitleLabel() {
 		final JLabel title = new JLabel("Upload Panel");
 		title.setHorizontalAlignment(SwingConstants.CENTER);
@@ -112,35 +110,49 @@ public class UploadPanel extends JPanel {
 		title.setBackground(Color.LIGHT_GRAY);
 		this.add(title, BorderLayout.NORTH);
 	}
-
+	
 	private void initLeftPanel() {
-		leftPanel = new UploadLeftPanel();
+		leftPanel = new UploadLeftPanel(this);
 		splitPane.setLeftComponent(leftPanel);
 	}
 	
 	private void initDisplayPanel() {
 		displayPanel = new JPanel(new BorderLayout());
-		
-		App.getInputProcessor().getInputNotifier().subscribe(event -> {
-			File file = event.getFiles()[0];
-			ScalableLabel label = new ScalableLabel(new ImageIcon(file.getPath()));
-			label.setMinimumSize(new Dimension(32, 32));
-			
-			displayPanel.removeAll();
-			displayPanel.add(label, BorderLayout.CENTER);
-			
-			App.getAppWindow().queueRepaint(displayPanel);
-			displayPanel.revalidate();
-			
-		}, EventType.JLIST_FOCUS_CHANGED);
+		displayPanel.add(new ScalableLabel(null));
 		splitPane.setRightComponent(displayPanel);
 	}
-
+	
 	private void initSplitPane() {
 		splitPane = new JSplitPane();
 		splitPane.setContinuousLayout(true);
 		add(splitPane);
 	}
 	
+	public void updateDisplayPanel(ImageIcon image) {
+		ScalableLabel label = (ScalableLabel) displayPanel.getComponent(0);
+		
+		label.setIcon(image);
+		App.getAppWindow().queueRepaint(label);
+		label.revalidate();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
