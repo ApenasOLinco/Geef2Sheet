dcf01bace62f7457cb0d88fbad3419e13141db37
ApenasOLinco <lincoln.matos1003@gmail.com>
2024-10-26 18:10:27 -0300

    Added text centralization to the spinners inside the Options Panel

--- src/ui/OptionsPanel.java
+++ src/ui/OptionsPanel.java
@@ -7,12 +7,17 @@ import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
+import javax.swing.JSpinner.DefaultEditor;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
+
+import io.OutputConfigurations;
 
 public class OptionsPanel extends JPanel {
 	JPanel contentPanel = new JPanel();
@@ -29,43 +34,54 @@ public class OptionsPanel extends JPanel {
 		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
 		contentPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		
-		initNumberOfColumnsSpinner();
-		initGapSpinners();
+		initSpinners();
 		
 		scrollPane.setViewportView(contentPanel);
 		add(scrollPane);
 	}
 	
-	private void initNumberOfColumnsSpinner() {
+	private void initSpinners() {
+		// Number Of Columns Spinner
 		createSpinner(numberOfColumnsSpinner, "Number of Columns: ");
-	}
-	
-	private void initGapSpinners() {
+		numberOfColumnsSpinner.setValue(OutputConfigurations.getNumberOfColumns());
+
 		// Horizontal Spinner
 		createSpinner(horizontalGapSpinner, "Horizontal Gap: ");
+		horizontalGapSpinner.setValue(OutputConfigurations.gethGap());
 		
 		// Vertical Spinner
 		createSpinner(verticalGapSpinner, "Vertical Gap: ");
+		verticalGapSpinner.setValue(OutputConfigurations.getvGap());
 	}
 	
 	private void createSpinner(JSpinner spinner, String labelText) {
-		Dimension spinnerMinimumSize = new Dimension(50, 25);
+		Dimension spinnerMinimumSize = new Dimension(25, 25);
 		Dimension spinnerMaximumSize = new Dimension(500, 25);
 		
-		SpinnerNumberModel gapModel = new SpinnerNumberModel();
-		gapModel.setMinimum(0);
+		SpinnerNumberModel model = new SpinnerNumberModel();
+		model.setMinimum(0);
 		
+		// Container
 		JPanel container = new JPanel();
-		container.setLayout(new BoxLayout(container, BoxLayout.LINE_AXIS));
+		BoxLayout layout = new BoxLayout(container, BoxLayout.LINE_AXIS);
+		container.setLayout(layout);
 		container.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
 		
-		spinner.setModel(gapModel);
+		spinner.setModel(model);
 		spinner.setMinimumSize(spinnerMinimumSize);
 		spinner.setMaximumSize(spinnerMaximumSize);
 		spinner.setPreferredSize(spinnerMaximumSize);
 		
+		// Centralize the text on the spinner
+		JComponent editor = spinner.getEditor();
+		
+		if(editor instanceof JSpinner.DefaultEditor) {
+			var defaultEditor = (DefaultEditor) editor;
+			JFormattedTextField textField = defaultEditor.getTextField();
+			textField.setHorizontalAlignment(SwingConstants.CENTER);
+		}
+		
 		JLabel label = new JLabel(labelText);
-		label.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		container.add(label);
 		container.add(Box.createHorizontalGlue());
