edb02f2750ef58c3816fd576a16885311b5fef3f
ApenasOLinco <lincoln.matos1003@gmail.com>
2025-04-27 22:40:47 -0300

    Basic main controller

--- src/main/java/com/apenasolinco/geef2sheet_api/controller/GifController.java
+++ src/main/java/com/apenasolinco/geef2sheet_api/controller/GifController.java
@@ -1,10 +1,22 @@
 package com.apenasolinco.geef2sheet_api.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
+import java.io.InputStream;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/gif")
 public class GifController {
-	
+
+	@PostMapping("/convert")
+	public ResponseEntity<Image> imageToGif(@RequestBody Image image) {
+		return ResponseEntity.ok()
+			.body(image);
+	}
 }
