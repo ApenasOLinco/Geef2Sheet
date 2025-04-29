c002edc07e38dae704bc2e45f99a060d7e39735a
ApenasOLinco <lincoln.matos1003@gmail.com>
2025-04-29 18:59:11 -0300

    Update gitignore cached files

--- src/main/java/com/apenasolinco/geef2sheet_api/controller/GifController.java
+++ src/main/java/com/apenasolinco/geef2sheet_api/controller/GifController.java
@@ -1,22 +1,22 @@
 package com.apenasolinco.geef2sheet_api.controller;
 
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.awt.*;
-import java.io.InputStream;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/gif")
 public class GifController {
-
 	@PostMapping("/convert")
-	public ResponseEntity<Image> imageToGif(@RequestBody Image image) {
+	public ResponseEntity<String> imageToGif(
+		@RequestParam("imagem") MultipartFile image
+	) {
 		return ResponseEntity.ok()
-			.body(image);
+			.body("Recebida " + image.getOriginalFilename() + "\n");
+	}
+
+	@GetMapping
+	public ResponseEntity<String> testAPI() {
+		return ResponseEntity.ok().body("TEST");
 	}
 }
