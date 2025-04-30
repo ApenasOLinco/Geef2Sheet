4123f46144c1c18ae56517cfee587c890a9e182c
ApenasOLinco <lincoln.matos1003@gmail.com>
2025-04-30 11:42:30 -0300

    Start ignoring editor-specific files

---
+++ src/main/java/com/apenasolinco/geef2sheet_api/controller/GifController.java
@@ -0,0 +1,24 @@
+package com.apenasolinco.geef2sheet_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/gif")
+public class GifController {
+
+	@PostMapping("/convert")
+	public ResponseEntity<String> imageToGif(
+		@RequestParam("imagem") MultipartFile image
+	) {
+		
+		return ResponseEntity.ok()
+			.body("Recebida " + image.getOriginalFilename() + "\n");
+	}
+
+	@GetMapping
+	public ResponseEntity<String> testAPI() {
+		return ResponseEntity.ok().body("TEST");
+	}
+}
