9d7fc8ba67fb8585fdccf04cf87aa0b6f5df2901
ApenasOLinco <lincoln.matos1003@gmail.com>
2025-05-11 19:30:25 -0300

    Changed OutputConfigurations from Record to @Data lombok class

--- src/main/java/com/apenasolinco/geef2sheet_api/model/OutputConfigurations.java
+++ src/main/java/com/apenasolinco/geef2sheet_api/model/OutputConfigurations.java
@@ -1,10 +1,12 @@
 package com.apenasolinco.geef2sheet_api.model;
 
-public record OutputConfigurations(
-	int numberOfColumns,
-	int verticalGap,
-	int horizontalGap,
-	String outputFormat
-) {
+import lombok.Data;
 
+@Data
+public class OutputConfigurations {
+	private int numberOfColumns,
+		verticalGap,
+		horizontalGap;
+
+	private String outputFormat;
 }
